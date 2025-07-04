e16d36d2310171e7e771ad375c0722db075645d3
GiorgioUll <giorgio.ulliana@gmail.com>
2025-07-04 15:24:26 +0200

    Create esercizio91.js

---
+++ esercizio91.js
@@ -0,0 +1,56 @@
+//esercizio91 - crea oggetto libro
+//creazione oggetto
+let libro = {
+    titolo: 'cucinare zucchine',
+    autore: 'giorgio',
+    pagine: 30,
+    letto: true,
+
+//metodi
+    stampalibro: function() {
+        console.log(this.libro); 
+    },
+    gialetto: function() {
+        this.letto == false;
+        console.log(libro.letto);
+    },
+    tempolettura: function() {
+        let gialetto = libro.pagine*2;
+        console.log(gialetto);
+    },
+};
+
+//    UTILIZZO
+//accesso alle proprieta'
+console.log(libro.titolo);
+
+//modifica proprieta' e stampa
+libro.titolo = "cucinare le zucchine";
+console.log('nuovo titolo: ' + libro.titolo);
+
+//stampa delle info:
+console.log('')
+console.log('  Informazioni sul libro aggiornate: ');
+console.log('titolo: ' + libro.titolo);
+console.log('autore: ' + libro.autore);
+console.log('pagine: ' + libro.pagine);
+console.log('tempo di lettura ' + (libro.pagine * 2) + ' minuti')
+if (libro.letto == true)
+  {  
+    console.log('il libro e stato letto')
+  } else {
+    console.log('il libro non e stato letto')
+  }
+
+//stampa delle info:
+console.log('')
+console.log('  e in sintesi: ');
+console.log(libro.stampalibro;
+console.log('tempo di lettura ' + (libro.pagine * 2) + ' minuti')
+if (libro.letto == true)
+  {  
+    console.log('il libro e stato letto')
+  } else {
+    console.log('il libro non e stato letto')
+  }
+
